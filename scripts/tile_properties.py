f689269cd1de45d5ba1c42554448418953cf3175
SimKDT <simon.cadet79@hotmail.fr>
2026-06-07 19:38:07 +0200

    Add data for tile properties

--- scripts/tile_properties.py
+++ scripts/tile_properties.py
@@ -2,12 +2,14 @@
 Parses 
 """
 
-import json, re
+import json, re, yaml
 from pathlib import Path
 
 JAVA = Path("ZomboidDecompiler/output/source/zombie/core/properties/TilePropertyKey.java")
 OUT = Path("out/tile_properties.json")
+DATA = Path("data/tile_properties.yaml")
 
+# parse the java file for tile properties
 PATTERN = r'public static final TilePropertyKey (?P<field>\w+) = registerBase\("(?P<name>\w+)"\);'
 
 with open(JAVA, "r") as f:
@@ -17,6 +19,16 @@ with open(JAVA, "r") as f:
     property = []
     for match in matches:
         property.append(match.groupdict())
+
+# parse the data file for tile properties
+with open(DATA, "r") as f:
+    data = yaml.safe_load(f)
+
+    for name, obj in data["objects"].items():
+        for prop in property:
+            if prop["name"] == name:
+                for key, value in obj.items():
+                    prop[key] = value
 
 with open(OUT, "w") as out_file:
     json.dump(property, out_file, indent=4)
